3fab8efc3773b5de7d26be23c5f54bbd648fae9e
Norbertas <norbertas.blandis@gmail.com>
2021-01-06 13:57:35 +0200

    Working moving pieces

--- chess.py
+++ chess.py
@@ -1,6 +1,7 @@
 from tkinter import Tk, Canvas, PhotoImage, Button
 
 
+
 def configure_game():
 	window.geometry("640x640")
 	window.title("C H E S S")
@@ -9,13 +10,25 @@ def configure_game():
 
 class Team:
 
-	def __init__(self, team_list, x_coord, y_coord, color, count, lives):
+	def __init__(self, team_list, x_coord, y_coord, color, count, lives, button):
 		self.list = team_list
 		self.x_coord = x_coord
 		self.y_coord = y_coord
 		self.color = color
 		self.count = count
 		self.lives = lives
+		self.button = button
+
+
+class Possible_moves:
+
+	def __init__(self, free_x_list, free_y_list, taken_x, taken_y, attack_x, attack_y):
+		self.free_x_list = free_x_list
+		self.free_y_list = free_y_list
+		self.taken_x = taken_x
+		self.taken_y = taken_y
+		self.attack_x = attack_x
+		self.attack_y = attack_y
 
 
 def check_square_color(y, x):
@@ -29,53 +42,145 @@ def check_square_color(y, x):
 	return temporary_image
 
 
-def move_player(old_x, old_y, new_y, new_x):
-	global possible_moves
+def click_coordinates(event, possible_move_x, possible_move_y, y, x, color, character, index, team):
+	click_x, click_y = event.x, event.y
+
+	#If the pawn was mooved for the first time, next move only 1 space
+	if(character == "pawn" and team.count[index] == 0):
+		team.count[index] += 1
+
+	#Looping through all possible moves and if click match, call move_player
+	for i in range(len(possible_move_x)):
+
+		possible_move_coord_x = possible_move_x[i]*80
+		possible_move_coord_y = possible_move_y[i]*80
+		if(click_x > possible_move_coord_x and click_x < possible_move_coord_x+80):
+			if(click_y > possible_move_coord_y and click_y < possible_move_coord_y+80):
+				move_player(y, x, possible_move_y[i], possible_move_x[i], color, character, index)
+
+
+def move_player(old_y, old_x, new_y, new_x, color, character_type, index):
+	print(old_x, old_y, " moves to ", new_x, new_y)
+
+	#Deleting possible moves squares
+	global possible_moves, black_team, white_team
+	print(len(possible_moves))
 	for x in range(len(possible_moves)):
 		game_canvas.delete(possible_moves[x])
 	window.update()
 	possible_moves = []
 
+	#Changing the piece coordinates to new ones
+	if(color == "black"):
+		team = black_team
+	else:
+		team = white_team
+	for i in range(16):
+		if(team.x_coord[i]==old_x and team.y_coord[i]==old_y):
+			game_canvas.delete(team.button[i])
+			team.x_coord[i] = new_x
+			team.y_coord[i] = new_y
 
-def pawn_pressed_1(team, other_team, y, x, direction, z):
+	#Checking which character button should be created
+	if(character_type == "pawn"):
+		image_1 = b_pawn_w
+		image_2 = b_pawn_b
+		image_3 = w_pawn_w
+		image_4 = w_pawn_b
+	elif(character_type == "knight"):
+		image_1 = b_knight_w
+		image_2 = b_knight_b
+		image_3 = w_knight_w
+		image_4 = w_knight_b
+	elif(character_type == "rook"):
+		image_1 = b_rook_w
+		image_2 = b_rook_b
+		image_3 = w_rook_w
+		image_4 = w_rook_b
+	elif(character_type == "bishop"):
+		image_1 = b_bishop_w
+		image_2 = b_bishop_b
+		image_3 = w_bishop_w
+		image_4 = w_bishop_b
+	elif(character_type == "queen"):
+		image_1 = b_queen_w
+		image_2 = b_queen_b
+		image_3 = w_queen_w
+		image_4 = w_queen_b
+	elif(character_type == "king"):
+		image_1 = b_king_w
+		image_2 = b_king_b
+		image_3 = w_king_w
+		image_4 = w_king_b
 
-	global free_move
-	new_y_coord = y
+	#Calling a function to display new place for the piece
+	if(color == "black"):
+		display_character(new_y, new_x, image_1, image_2, character_type, color, index)
+	elif(color == "white"):
+		display_character(new_y, new_x, image_3, image_4, character_type, color, index)
+	window.update()
+
+
+def pawn_pressed_1(team, other_team, y, x, direction, z, index):
+
+	global free_move, possible_moves
+	new_y_coord, new_x_coord = [], []
 
 	#Checking if it is the first move
-	if(team.count == 0):
+	if(team.count[index] == 0):
 		times = 2
 	else:
 		times = 1
 
+	possible_moves = [None]*times
 	#Looking at all possible moves for the pawn
 	for i in range(times):
-		new_y_coord = new_y_coord + direction
 
+		new_x_coord.append(x)
+
+		if(i == 0):
+			new_y_coord.append(y + direction)
+		else:
+			new_y_coord.append(new_y_coord[i-1] + direction)
+
+		#Looping through all pieces and checking if another one is encountered
 		for j in range(16):
 
-			if(new_y_coord==team.y_coord[z] and x==team.x_coord[z]):
-				print("Space taken at ", x, new_y_coord)
-				free_move = False
-			elif(new_y_coord==other_team.y_coord[z] and x==other_team.x_coord[z]):
-				print("Can attack at ", x, new_y_coord)
+			if(new_y_coord[i]==team.y_coord[z] and x==team.x_coord[z]):
+				print("Space taken at ", x, new_y_coord[i])
 				free_move = False
 
-		if not free_move:
+			elif(new_y_coord[i]==other_team.y_coord[z] and x==other_team.x_coord[z]):
+				print("Can attack at ", x, new_y_coord[i])
+				free_move = False
+
+			elif(new_y_coord[i]>7 or new_y_coord[i]<0 or new_x_coord[i]>7 or new_x_coord[i]<0):
+				print("Out of bounds")
+				free_move = False
+				break
+
+		if not free_move: #If the pawn cannot move exit the function
 			return None
+
+
+	#Binding key press to check if possible move was pressed
+	game_canvas.bind("<Button-1>", lambda event: click_coordinates(event, new_x_coord, new_y_coord, y, x, team.color, "pawn", index, team))
+
+	#Creating green squares for possible moves
+	for i in range(times):
+
+		if(check_square_color(new_x_coord[i], new_y_coord[i]) == "black"):
+			temporary_image = green_b
 		else:
-			#If the space is free
-			if(check_square_color(new_y_coord, x) == "black"):
-				temporary_image = green_b
-			else:
-				temporary_image = green_w
-			print("Free space at ", x, new_y_coord)
-			temporary_button = Button(window, image=temporary_image, command=lambda:move_player(y, x, new_y_coord, x))
-			possible_moves.append(game_canvas.create_window(40+x*80,40+new_y_coord*80, window = temporary_button))
-			window.update()
+			temporary_image = green_w
+
+		possible_moves[i] = game_canvas.create_image(80*new_x_coord[i], 80*new_y_coord[i], image = temporary_image, anchor = "nw")
+		window.update()
 
 
-def pawn_pressed(y, x):
+def pawn_pressed(y, x, color, index):
+
+	print("Pawn pressed")
 
 	#Clearing current possible moves squares for new options when clicked
 	global possible_moves
@@ -92,33 +197,161 @@ def pawn_pressed(y, x):
 	#Looking if the selected pawn is white or black
 	for z in range(16):
 		if(y==black_team.y_coord[z] and x==black_team.x_coord[z]):
-			pawn_pressed_1(black_team, white_team, y, x, 1, z)
+			pawn_pressed_1(black_team, white_team, y, x, 1, z, index)
 
 		if(y==white_team.y_coord[z] and x==white_team.x_coord[z]):
-			pawn_pressed_1(white_team, black_team, y, x, -1, z)
+			pawn_pressed_1(white_team, black_team, y, x, -1, z, index)
 
 		#If there are no free moves, then quit searching for more moves
 		if not free_move:
 			break
 
 
-def rook_pressed():
-	print("Rook pressed")
+def rook_possible_moves(i, temporary_y_coord, temporary_x_coord, team, opp_team, direction):
+
+	rook_move = Possible_moves([], [], None, None, None, None)
+
+	#Figuring out if the rook moves vertically or horizontally
+	is_free = True
+	if direction == "y":
+		add_to_y = i
+		add_to_x = 0
+	else:
+		add_to_y = 0
+		add_to_x = i
+
+	#Looping through possible move options until a problem encountered
+	
+	while is_free:
+		temporary_y_coord += add_to_y
+		temporary_x_coord += add_to_x
+		for z in range(16):
+			if(temporary_y_coord==team.y_coord[z] and temporary_x_coord==team.x_coord[z]):
+				print("Space taken at", temporary_x_coord, temporary_y_coord)
+				rook_move.taken_x = temporary_x_coord
+				rook_move.taken_y = temporary_y_coord
+				is_free = False
+				break
+
+			if(temporary_y_coord==opp_team.y_coord[z] and temporary_x_coord==opp_team.x_coord[z]):
+				print("Can attack at", temporary_x_coord, temporary_y_coord)
+				rook_move.attack_x = temporary_x_coord
+				rook_move.attack_y = temporary_y_coord
+				is_free = False
+				break
+
+			if(temporary_y_coord>7 or temporary_y_coord<0 or temporary_x_coord>7 or temporary_x_coord<0):
+				print("Out of bounds")
+				is_free = False
+				break
+
+		if is_free:
+			print("Space free at", temporary_x_coord, temporary_y_coord)
+			rook_move.free_x_list.append(temporary_x_coord)
+			rook_move.free_y_list.append(temporary_y_coord)
+
+	return rook_move
 
 
-def knight_pressed():
+def rook_pressed(y, x, color, index):
+
+	#Deleting possible moves squares
+	global possible_moves
+	print(len(possible_moves))
+	for x in range(len(possible_moves)):
+		game_canvas.delete(possible_moves[x])
+	window.update()
+	possible_moves = []
+
+	#Finding out exactly which piece was pressed
+	if(color == "black"):
+		team = black_team
+		opp_team = white_team
+		direction = [1, 1, -1, -1] #1-down, 2-right, 3-up, 4-right
+	else:
+		team = white_team
+		opp_team = black_team
+		direction = [1, 1, -1, -1] #1-down, 2-right, 3-up, 4-right
+
+	#Creating a list and adding to it objects that contain move info
+	rook_moves = []
+	print("Checking downwards")
+	rook_moves.append(rook_possible_moves(direction[0], y, x, team, opp_team, "y"))
+	print()
+	print("Checking to the right")
+	rook_moves.append(rook_possible_moves(direction[1], y, x, team, opp_team, "x"))
+	print()
+	print("Checking upwards")
+	rook_moves.append(rook_possible_moves(direction[2], y, x, team, opp_team, "y"))
+	print()
+	print("Checking to the left")
+	rook_moves.append(rook_possible_moves(direction[3], y, x, team, opp_team, "x"))
+	print()
+
+	#Adding possible moves coordinates to new lists
+	possible_move_x, possible_move_y = [], []
+	for i in range(len(rook_moves)):
+
+		#Adding possible free moves to the list
+		if(len(rook_moves[i].free_x_list) > 0):
+			for z in range(len(rook_moves[i].free_x_list)):
+				possible_moves.append(None)
+				possible_move_x.append(rook_moves[i].free_x_list[z])
+				possible_move_y.append(rook_moves[i].free_y_list[z])
+
+
+	#Binding key press to check if possible move was pressed
+	game_canvas.bind("<Button-1>", lambda event: click_coordinates(event, possible_move_x, possible_move_y, y, x, color, "rook", index, team))
+
+	#Creating green squares for possible moves
+	for i in range(len(possible_moves)):
+
+		if(check_square_color(possible_move_x[i], possible_move_y[i]) == "black"):
+			temporary_image = green_b
+		else:
+			temporary_image = green_w
+
+		possible_moves[i] = game_canvas.create_image(80*possible_move_x[i], 80*possible_move_y[i], image = temporary_image, anchor = "nw")
+		window.update()
+
+
+def knight_pressed(y, x, color, index):
+	if(color == "black"):
+		direction = 1
+		team = black_team
+	else:
+		direction = -1
+		team = white_team
 	print("Knight pressed")
 
 
-def bishop_pressed():
+def bishop_pressed(y, x, color, index):
+	if(color == "black"):
+		direction = 1
+		team = black_team
+	else:
+		direction = -1
+		team = white_team
 	print("Bishop pressed")
 
 
-def queen_pressed():
+def queen_pressed(y, x, color, index):
+	if(color == "black"):
+		direction = 1
+		team = black_team
+	else:
+		direction = -1
+		team = white_team
 	print("Queen pressed")
 
 
-def king_pressed():
+def king_pressed(y, x, color, index):
+	if(color == "black"):
+		direction = 1
+		team = black_team
+	else:
+		direction = -1
+		team = white_team
 	print("King pressed")
 
 
@@ -156,7 +389,7 @@ def create_buttons():
 
 def create_team(team):
 	for x in range(8):
-		team.list[x] = "private"
+		team.list[x] = "pawn"
 		team.x_coord[x] = x
 		if(team.color == "black"):
 			team.y_coord[x] = 1
@@ -164,7 +397,7 @@ def create_team(team):
 			team.y_coord[x] = 6
 
 	for x in range(8, 10):
-		team.list[x] = "tower"
+		team.list[x] = "rook"
 		if(team.color == "black"):
 			team.y_coord[x] = 0
 		else:
@@ -174,7 +407,7 @@ def create_team(team):
 
 
 	for x in range(10, 12):
-		team.list[x] = "horse"
+		team.list[x] = "knight"
 		if(team.color == "black"):
 			team.y_coord[x] = 0
 		else:
@@ -209,7 +442,7 @@ def create_team(team):
 	team.x_coord[15] = 4
 
 
-def display_character(y, x, image_1, image_2, character_type):
+def display_character(y, x, image_1, image_2, character_type, color, index):
 
 	#Converting coordinates into pixels in the canvas
 	temporary_x_coord = 40 + x*80
@@ -222,11 +455,11 @@ def display_character(y, x, image_1, image_2, character_type):
 		temporary_image = image_2
 
 	#Checking which character button should be created
-	if(character_type == "private"):
+	if(character_type == "pawn"):
 		temporary_command = pawn_pressed
-	elif(character_type == "horse"):
+	elif(character_type == "knight"):
 		temporary_command = knight_pressed
-	elif(character_type == "tower"):
+	elif(character_type == "rook"):
 		temporary_command = rook_pressed
 	elif(character_type == "bishop"):
 		temporary_command = bishop_pressed
@@ -235,42 +468,50 @@ def display_character(y, x, image_1, image_2, character_type):
 	elif(character_type == "king"):
 		temporary_command = king_pressed
 
-	#Creating and displaying the button
-	temporary_button = Button(window, image = temporary_image, height = "80", width = "80", command = lambda:temporary_command(y, x))
-	board_button=game_canvas.create_window(temporary_x_coord, temporary_y_coord, window = temporary_button)
+	#Creating the button
+	temporary_button = Button(window, image = temporary_image, height = "80", width = "80", command = lambda:temporary_command(y, x, color, index))
+
+	#Adding the created button to the team class
+	if(color == "black"):
+		team = black_team
+	else:
+		team = white_team
+	for i in range(16):
+		if(team.x_coord[i]==x and team.y_coord[i]==y):
+			team.button[i] = game_canvas.create_window(temporary_x_coord, temporary_y_coord, window = temporary_button)
 	window.update()
 
 
 def display_team(team):
 	#If the figure is the rook calling the display character function
 	for i in range(len(team.list)):
-		if(team.list[i]=="tower"):
+		if(team.list[i]=="rook"):
 			x = team.x_coord[i]
 			y = team.y_coord[i]
 			if(team.color == "black"):
-				display_character(y, x, b_rook_w, b_rook_b, team.list[i])
+				display_character(y, x, b_rook_w, b_rook_b, team.list[i], team.color, i)
 			elif(team.color == "white"):
-				display_character(y, x, w_rook_w, w_rook_b, team.list[i])
+				display_character(y, x, w_rook_w, w_rook_b, team.list[i], team.color, i)
 
 	#If the figure is the pawn calling the display character function
 	for i in range(len(team.list)):
-		if(team.list[i]=="private"):
+		if(team.list[i]=="pawn"):
 			x = team.x_coord[i]
 			y = team.y_coord[i]
 			if(team.color == "black"):
-				display_character(y, x, b_pawn_w, b_pawn_b, team.list[i])
+				display_character(y, x, b_pawn_w, b_pawn_b, team.list[i], team.color, i)
 			elif(team.color == "white"):
-				display_character(y, x, w_pawn_w, w_pawn_b, team.list[i])
+				display_character(y, x, w_pawn_w, w_pawn_b, team.list[i], team.color, i)
 
 	#If the figure is the knight calling the display character function
 	for i in range(len(team.list)):
-		if(team.list[i]=="horse"):
+		if(team.list[i]=="knight"):
 			x = team.x_coord[i]
 			y = team.y_coord[i]
 			if(team.color == "black"):
-				display_character(y, x, b_knight_w, b_knight_b, team.list[i])
+				display_character(y, x, b_knight_w, b_knight_b, team.list[i], team.color, i)
 			elif(team.color == "white"):
-				display_character(y, x, w_knight_w, w_knight_b, team.list[i])
+				display_character(y, x, w_knight_w, w_knight_b, team.list[i], team.color, i)
 
 	#If the figure is the bishop calling the display character function
 	for i in range(len(team.list)):
@@ -278,9 +519,9 @@ def display_team(team):
 			x = team.x_coord[i]
 			y = team.y_coord[i]
 			if(team.color == "black"):
-				display_character(y, x, b_bishop_w, b_bishop_b, team.list[i])
+				display_character(y, x, b_bishop_w, b_bishop_b, team.list[i], team.color, i)
 			elif(team.color == "white"):
-				display_character(y, x, w_bishop_w, w_bishop_b, team.list[i])
+				display_character(y, x, w_bishop_w, w_bishop_b, team.list[i], team.color, i)
 
 	#If the figure is the queen calling the display character function
 	for i in range(len(team.list)):
@@ -288,9 +529,9 @@ def display_team(team):
 			x = team.x_coord[i]
 			y = team.y_coord[i]
 			if(team.color == "black"):
-				display_character(y, x, b_queen_w, b_queen_b, team.list[i])
+				display_character(y, x, b_queen_w, b_queen_b, team.list[i], team.color, i)
 			elif(team.color == "white"):
-				display_character(y, x, w_queen_w, w_queen_b, team.list[i])
+				display_character(y, x, w_queen_w, w_queen_b, team.list[i], team.color, i)
 
 	#If the figure is the king calling the display character function
 	for i in range(len(team.list)):
@@ -298,15 +539,17 @@ def display_team(team):
 			x = team.x_coord[i]
 			y = team.y_coord[i]
 			if(team.color == "black"):
-				display_character(y, x, b_king_w, b_king_b, team.list[i])
+				display_character(y, x, b_king_w, b_king_b, team.list[i], team.color, i)
 			elif(team.color == "white"):
-				display_character(y, x, w_king_w, w_king_b, team.list[i])
+				display_character(y, x, w_king_w, w_king_b, team.list[i], team.color, i)
 
 
 def mainloop():
 
 	#Create the background game canvas
 	create_buttons()
+
+	game_canvas.bind("<Button-1>", click_coordinates)
 
 	#Create white and black teams
 	create_team(black_team)
@@ -317,12 +560,14 @@ def mainloop():
 	display_team(white_team)
 
 
+
 window = Tk()
 
 #Defining team variables
-black_team = Team([None]*16, [None]*16, [None]*16, "black", 0, [1]*16)
-white_team = Team([None]*16, [None]*16, [None]*16, "white", 0, [1]*16)
+black_team = Team([None]*16, [None]*16, [None]*16, "black", [0]*16, [1]*16, [None]*16)
+white_team = Team([None]*16, [None]*16, [None]*16, "white", [0]*16, [1]*16, [None]*16)
 possible_moves = []
+click_x, click_y = 0, 0
 
 #Upload background images
 white_square = PhotoImage(file = "white_square.png")
